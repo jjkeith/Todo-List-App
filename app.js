06e2c954922f6268abfa6ed3e7882ed7b4b89d6c
JJ <jj@jjkeith.net>
2016-07-27 09:48:44 -0700

    Created sample todo list to load with app

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function TaskControllerFunc($http) {
 
 
 
-  this.todoList = ['Anne of Green Gables', "Ender's Game", 'Song of Fire and Ice', 'Feast for Crows', 'HTML for Dummies', "Hitchhiker's Guide to the Galaxy", "So Long and Thanks for All the Fish" ]
+  this.todoList = ['Buy cat food', 'Oil change', 'Take curtains to the cleaner', 'Buy present for party',]
   this.addTask = function(taskName) {
     this.todoList.push(taskName)
     this.newTask = ''
